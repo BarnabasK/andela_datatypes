973f2ec39a8c3e2721a3ee668f9453e272a377f4
BarnabasK <kariukibarnabas@gmail.com>
2016-10-30 19:25:26 +0300

    Second Commit

--- factorial.py
+++ factorial.py
@@ -1,4 +1,6 @@
-import unittest
+#Have a function factorial(number), take the number parameter been passed and return the factorial of it.
+#For example: if number is 4, it should return (4 * 3 * 2 * 1) which is 24.
+
 def factorial(num):
 	factorial = 1
 	for i in range(1, num+1):
